6a05f1f1b11c92b9e09c2393e52852a1948da35d
0xurandom <225617807+0xurandom@users.noreply.github.com>
2026-07-14 22:54:41 +0530

    partially implement cartdata and api

---
+++ src/backend/pico8/api/api.hpp
@@ -0,0 +1,27 @@
+// game loop
+
+// math
+#include <cstdint>
+
+int32_t p_abs(int32_t x);
+int32_t p_atan2(int32_t dx, int32_t dy);
+int32_t p_band(int32_t x, int32_t y);
+int32_t p_bnot(int32_t x);
+int32_t p_bor(int32_t x, int32_t y);
+int32_t p_bxor(int32_t x, int32_t y);
+int32_t p_ceil(int32_t x);
+int32_t p_cos(int32_t x);
+int32_t p_flr(int32_t x);
+int32_t p_lshr(int32_t n, int32_t bits);
+int32_t p_max(int32_t x, int32_t y = 0);
+int32_t p_mid(int32_t x, int32_t y, int32_t z);
+int32_t p_min(int32_t x, int32_t y = 0);
+int32_t p_rnd();  // not working yet
+int32_t p_rotl(int32_t n, int32_t bits);
+int32_t p_rotr(int32_t n, int32_t bits);
+int32_t p_sgn(int32_t x);
+int32_t p_shl(int32_t n, int32_t bits);
+int32_t p_shr(int32_t n, int32_t bits);
+int32_t p_sin(int32_t x);
+int32_t p_sqrt(int32_t x);
+int32_t p_srand(int32_t x);  // also not working
---
+++ src/backend/pico8/api/cartdata.cpp
@@ -0,0 +1,82 @@
+#include <cstdint>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <system_error>
+
+#include "api.hpp"
+using namespace std;
+
+bool p_cartdata(string id) {}
+
+filesystem::path getSaveDir() {
+#ifdef __linux__
+    if (const char* xdg = getenv("XDG_CONFIG_HOME")) {
+        return filesystem::path(xdg) / "polyemu";
+    }
+
+    const char* home = std::getenv("HOME");
+    return filesystem::path(home) / ".config" / "polyemu";
+#endif
+}
+
+bool saveDirExists() {
+    filesystem::path dir = getSaveDir();
+
+    error_code ec;
+    if (!filesystem::exists(dir)) {
+        if (!filesystem::create_directories(dir, ec)) {
+            cerr << "Error: Could not create save directory: " << ec.message()
+                 << endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+bool writeCartData(const string& id, uint8_t index, uint8_t data) {
+    if (!saveDirExists() || index > 64) return false;
+
+    filesystem::path filePath = getSaveDir() / (id + ".txt");
+
+    ofstream out(filePath, ios::in | ios::out);
+
+    if (!out.is_open()) {
+        cerr << "Error: File count not be opened\n";
+        return false;
+    }
+
+    uintmax_t size = filesystem::file_size(filePath);
+    if (size != 64) {
+        filesystem::resize_file(filePath, 64);
+    }
+
+    out.seekp(index, ios::beg);
+    out << data;
+    out.close();
+    return true;
+}
+
+uint8_t readCartData(const string& id, uint8_t index) {
+    if (!saveDirExists() || index > 64) return false;
+
+    filesystem::path filePath = getSaveDir() / (id + ".txt");
+    if (!filesystem::exists(filePath)) {
+        cerr << "Warning: Attempted to read cartdata which does not exist for "
+                "id: "
+             << id << endl;
+        return 0;
+    }
+
+    ifstream in(filePath, ios::binary);
+
+    if (!in) {
+        cerr << "Error: file could not be opened: " << filePath << endl;
+        return 0;
+    }
+
+    uint8_t data;
+    in.seekg(index, ios::beg);
+}
